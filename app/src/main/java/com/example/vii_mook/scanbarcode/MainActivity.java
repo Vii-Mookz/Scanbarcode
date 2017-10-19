a63a1c0dc205154f3279782d5dc7d359a3e0597a
vii-mook <vvimannin@gmail.com>
2017-10-19 15:19:25 +0700

    Scan BARCODE AND QR CODE ADJUST DESIGH

--- app/src/main/java/com/example/vii_mook/scanbarcode/MainActivity.java
+++ app/src/main/java/com/example/vii_mook/scanbarcode/MainActivity.java
@@ -10,12 +10,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.zxing.Result;
-
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
-
-import static android.R.attr.data;
-import static android.R.attr.handle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         txtResult = (TextView) findViewById(R.id.txtResult);
 
@@ -41,70 +36,3 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
-//public class MainActivity extends AppCompatActivity  implements ZXingScannerView.ResultHandler{
-//
-//    private ZXingScannerView mScannerView;
-//    private TextView txtResult;
-//    private Button button;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        txtResult = (TextView) findViewById(R.id.txtResult);
-//        button = (Button) findViewById(R.id.btn_scan);
-//    }
-//    public void QrScanner(View view){
-//
-//
-//        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
-//        setContentView(mScannerView);
-//
-//        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
-//        mScannerView.startCamera();         // Start camera
-//
-//    }
-//
-//    public void handleResult(Result rawResult) {
-//
-//        // Do something with the result here
-//        String contents = rawResult.getText().toString();
-//        Log.e("handler", rawResult.getText()); // Prints scan results
-//        Log.e("handler", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode)
-//        Log.d("BARCODE ==>" , contents);
-//
-//        // show the scanner result into dialog box.
-////        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-////        builder.setTitle("Scan Result");
-////        builder.setMessage("Result : " + rawResult.getText());
-////        AlertDialog alert1 = builder.create();
-////        alert1.show();
-//        startActivity(new Intent(this,MainActivity.class));
-//        MainActivity.this.txtResult.setText(contents);
-//        onBackPressed();
-////
-//
-////
-////
-////
-//////        this.txtResult.setText("Result : " +content);
-////this.txtResult.setText(contents);
-//        Toast.makeText(this,"Result :" + rawResult.getText(),Toast.LENGTH_LONG).show();
-////        this.finish();
-//        // If you would like to resume scanning, call this method below:
-////         mScannerView.resumeCameraPreview(this);
-//
-//
-//    }
-//
-////
-////    @Override
-////    public void onPause() {
-////        super.onPause();
-////        mScannerView.stopCamera();           // Stop camera on pause
-////    }
-//
-//
-//
-//
-//}
