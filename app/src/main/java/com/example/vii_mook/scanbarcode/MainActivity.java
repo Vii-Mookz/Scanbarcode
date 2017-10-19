b25a486b2fe4b1a679a2b5659f81ecf78301a313
vii-mook <vvimannin@gmail.com>
2017-10-19 10:43:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/vii_mook/scanbarcode/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.vii_mook.scanbarcode;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+
+
+public class MainActivity extends AppCompatActivity  implements ZXingScannerView.ResultHandler{
+
+    private ZXingScannerView mScannerView;
+    private TextView txtResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtResult = (TextView) findViewById(R.id.txtResult);
+
+    }
+
+    public void QrScanner(View view){
+
+
+        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+        setContentView(mScannerView);
+
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();         // Start camera
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+
+
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+
+        Log.e("handler", rawResult.getText()); // Prints scan results
+        Log.e("handler", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode)
+
+        // show the scanner result into dialog box.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setMessage("Result : " + rawResult.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();
+
+//        Toast.makeText(this,"Result :" + rawResult.getText(),Toast.LENGTH_LONG).show();
+        // If you would like to resume scanning, call this method below:
+         mScannerView.resumeCameraPreview(this);
+    }
+}
